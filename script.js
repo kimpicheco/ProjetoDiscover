1efe5875922bc0d0ae75b0ff3f2865f236581e55
kim <kimpicheco@gmail.com>
2023-07-09 04:55:25 -0300

    AboutMe, mudança de Font e algumas modificaçoes de UX

--- script.js
+++ script.js
@@ -10,10 +10,34 @@ function toggleMode() {
     img.setAttribute("alt", "Foto de perfil do Kim.")
   }
 }
+let aboutMe = document.getElementById("aboutMe")
+function openAboutMe() {
+  aboutMe.classList.add("open-aboutMe")
+}
+function closeAboutMe() {
+  aboutMe.classList.remove("open-aboutMe")
+}
+
 let popup = document.getElementById("popUP")
 function openPopup() {
   popup.classList.add("open-popup")
 }
 function closePopup() {
   popup.classList.remove("open-popup")
+}
+function alrt() {
+  alert("Em construção!")
+}
+document.querySelectorAll(".image-container img").forEach((image) => {
+  image.onclick = () => {
+    document.querySelector(".popUPimg").style.display = "block"
+    document.querySelector(".xpams").style.display = "block"
+    document.querySelector(".popup-bgfade").style.display = "block"
+    document.querySelector(".popUPimg img").src = image.getAttribute("src")
+  }
+})
+document.querySelector(".xpams").onclick = () => {
+  document.querySelector(".popUPimg").style.display = "none"
+  document.querySelector(".xpams").style.display = "none"
+  document.querySelector(".popup-bgfade").style.display = "none"
 }
